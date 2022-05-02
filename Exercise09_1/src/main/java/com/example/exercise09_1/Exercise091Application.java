5424aa590b13db59aa829a437954cf7ef8c6d215
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-05-02 13:18:44 -0500

    add to github

---
+++ Exercise09_1/src/main/java/com/example/exercise09_1/Exercise091Application.java
@@ -0,0 +1,13 @@
+package com.example.exercise09_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise091Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise091Application.class, args);
+    }
+
+}
---
+++ Exercise09_1/src/test/java/com/example/exercise09_1/Exercise091ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercise09_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise091ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
